8be26a11754696f00501004910c55acb0e42c2fd
calebpara <calebpara@gmail.com>
2022-10-06 15:45:14 -0500

    buil2

---
+++ dist/components/Macha.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const Macha = props => {
+  return /*#__PURE__*/_react.default.createElement("div", {
+    style: {
+      height: "200px",
+      width: "200px",
+      backgroundColor: "green"
+    }
+  }, /*#__PURE__*/_react.default.createElement("p", null, "Macha"));
+};
+
+var _default = Macha;
+exports.default = _default;
--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,15 @@ Object.defineProperty(exports, "Button", {
     return _Button.default;
   }
 });
+Object.defineProperty(exports, "Macha", {
+  enumerable: true,
+  get: function get() {
+    return _Macha.default;
+  }
+});
 
 var _Button = _interopRequireDefault(require("./components/Button"));
+
+var _Macha = _interopRequireDefault(require("./components/Macha"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
